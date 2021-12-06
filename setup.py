0b889fa0c05a0bd09d907282d4d798bdc84985d1
jameholme <39424878+jameholme@users.noreply.github.com>
2021-12-06 16:40:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 # Setup.py
 # By: James Holmes
+# Installs required pip packages
 # Creates files with required IDs and Keys for GNAAT.py
+
+import subprocess
+import sys
+
+print("Installing required pip packages...")
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "requests", "datetime"])
+
+print("Please provide the following values...")
 
 S3_Bucket = input("S3 Bucket Name: ")
 with open("S3_Bucket", "w") as f:
